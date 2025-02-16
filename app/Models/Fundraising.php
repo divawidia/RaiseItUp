a6c3e46ea5bf62514499098ac26f74a309a404c4
divawidia <wiartha2001@gmail.com>
2025-02-16 00:00:00 +0000

    feat : add fundraising model attribute

--- app/Models/Fundraising.php
+++ app/Models/Fundraising.php
@@ -2,9 +2,47 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fundraising extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'fundraiser_id',
+        'category_id',
+        'is_active',
+        'has_finished',
+        'name',
+        'slug',
+        'thumbnail',
+        'about',
+        'target_amount',
+    ];
+
+    public function fundraiser()
+    {
+        return $this->belongsTo(Fundraiser::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function phases()
+    {
+        return $this->hasMany(FundraisingPhase::class);
+    }
+
+    public function donations()
+    {
+        return $this->hasMany(Donatur::class);
+    }
+
+    public function withdrawals()
+    {
+        return $this->hasMany(FundraisingWithdrawal::class);
+    }
 }
