d3978a9b5bfcf64935add476211964868871a919
divawidia <wiartha2001@gmail.com>
2025-02-16 00:00:00 +0000

    feat (FundraisingWithdrawal model) : add fillable collumns and its relationship

--- app/Models/FundraisingWithdrawal.php
+++ app/Models/FundraisingWithdrawal.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FundraisingWithdrawal extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'fundraising_id',
+        'fundraiser_id',
+        'has_received',
+    ];
+
+    public function fundraising()
+    {
+        return $this->belongsTo(Fundraising::class);
+    }
+
+    public function fundraiser()
+    {
+        return $this->belongsTo(Fundraiser::class);
+    }
 }
