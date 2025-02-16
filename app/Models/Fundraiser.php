e0dcd84ea014d31d7822c5ebfc88cf98ed3a8d9b
divawidia <wiartha2001@gmail.com>
2025-02-16 00:00:00 +0000

    feat (Fundraiser model) : add fillable collumns and its relationship

--- app/Models/Fundraiser.php
+++ app/Models/Fundraiser.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fundraiser extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name', // Assuming fundraisers have a name or other fields
+    ];
+
+    public function fundraisings()
+    {
+        return $this->hasMany(Fundraising::class);
+    }
 }
