8e9cf36e7599c10e77051dabd97121541ddd7b5c
divawidia <wiartha2001@gmail.com>
2025-02-16 00:00:00 +0000

    feat (FundraisingPhase model) : add fillable collumns and its relationship

--- app/Models/FundraisingPhase.php
+++ app/Models/FundraisingPhase.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FundraisingPhase extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'fundraising_id',
+        'name',
+        'photo',
+        'notes',
+    ];
+
+    public function fundraising()
+    {
+        return $this->belongsTo(Fundraising::class);
+    }
 }
