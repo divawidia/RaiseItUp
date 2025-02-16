f8b466bf6e22b1c98b47dd8c4ba20ab7ce618e2f
divawidia <wiartha2001@gmail.com>
2025-02-16 00:00:00 +0000

    feat (Donor model) : add fillable collumns and its relationship

--- app/Models/Donor.php
+++ app/Models/Donor.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Donor extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'fundraising_id',
+        'total_amount',
+        'notes',
+        'is_paid',
+        'proof',
+    ];
+
+    public function fundraising()
+    {
+        return $this->belongsTo(Fundraising::class);
+    }
 }
