dac55c19216fe9847db4c826782030dc3c9ee8a4
divawidia <wiartha2001@gmail.com>
2025-02-17 00:00:00 +0000

    refactor (fundraising_withdrawal model) : add softdelete

--- app/Models/FundraisingWithdrawal.php
+++ app/Models/FundraisingWithdrawal.php
@@ -4,15 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FundraisingWithdrawal extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'fundraising_id',
-        'fundraiser_id',
+        'proof',
+        'bank_name',
+        'bank_account_number',
+        'bank_account_name',
+        'amount_requested',
+        'amount_received',
         'has_received',
+        'has_sent',
+        'fundraiser_id',
+        'fundraising_id',
     ];
 
     public function fundraising()
